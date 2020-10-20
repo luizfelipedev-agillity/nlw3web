73616b7e0438aae67945e243f2ff8f07343c3989
luizfelipedev-agillity <73117542+luizfelipedev-agillity@users.noreply.github.com>
2020-10-20 04:26:58 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
+import { BrowserRouter , Route } from 'react-router-dom';
 import Landing from './pages/landing';
 import Orphanegermap from './pages/orphanegermap';
 import Orphanage from './pages/Orphanage';
